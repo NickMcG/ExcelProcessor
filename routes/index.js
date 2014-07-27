fd5add87e9a54c01e2f12a3fdd3edd35a8924dda
Nick McGinness <ragnerd@gmail.com>
2014-07-26 20:21:15 -0500

    Basic app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* POST file */
+router.post('/uploadFile', function (req, res) {
+	var fstream;
+	req.pipe(req.busboy);
+	req.busboy.on('file', function (fieldname, file, filename) {
+		console.log('Uploading: ' + filename);
+		var tempPath = path.join(uploadPath, new Date().getTime().toString()),
+			serverFile = path.join(tempPath, filename);
+		fs.mkdirSync(tempPath);
+		fstream = fs.createWriteStream(serverFile);
+		file.pipe(fstream);
+		fstream.on('close', function () {
+			res.redirect('back');
+		});
+	});
 });
 
 module.exports = router;
