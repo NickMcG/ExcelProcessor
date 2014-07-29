13b1523f4bf478c06b4f56d023f4ef2a9e0e7d3f
Nick McGinness <ragnerd@gmail.com>
2014-07-28 20:09:01 -0500

    Got basic configuration page built

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,51 @@
 var router = require('express').Router(),
 	fs = require('fs'),
 	path = require('path'),
-	uploadPath = path.join(process.cwd(), 'uploads')/*,
-	excelHelper = require('../excelHelper')*/;
+	uploadPath = path.join(process.cwd(), 'uploads'),
+	excelHelper = require('../excelHelper');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Excel Processor', 'missingSheetName': req.query.missingSheetName });
+	var missingSheetName = req.query.missingSheetName || 'false',
+		missingFile = req.query.missingFile || 'false';
+
+	res.render('index', {
+	  'title': 'Excel Processor',
+	  'missingSheetName': missingSheetName.toLowerCase() === 'true',
+	  'missingFile': missingFile.toLowerCase() === 'true'
+	});
 });
 
 /* POST file */
 router.post('/uploadFile', function (req, res) {
-	if (!req.body.sheetName) {
-		res.redirect('/?missingSheetName=true');
-	}
-	else {
-		var fstream;
-		req.pipe(req.busboy);
-		req.busboy.on('file', function (fieldname, file, filename) {
-			var tempPath = path.join(uploadPath, new Date().getTime().toString()),
-				serverFile = path.join(tempPath, filename);
-			fs.mkdirSync(tempPath);
-			fstream = fs.createWriteStream(serverFile);
-			file.pipe(fstream);
-			fstream.on('close', function () {
-				// TODO: extract headers from the file
-				// TODO: Render a page which lets the user select which columns to manipulate
-				res.redirect('back');
-			});
+	var gotFile = false,
+		tempPath = path.join(uploadPath, new Date().getTime().toString()),
+		configureParams = { 'title': 'Configure Columns', 'tempPath': tempPath };
+	req.busboy.on('file', function (fieldname, file, filename) {
+		var fstream,
+			serverFile = path.join(tempPath, filename);
+		configureParams.file = serverFile;
+		gotFile = true;
+
+		fs.mkdirSync(tempPath);
+		fstream = fs.createWriteStream(serverFile);
+		file.pipe(fstream);
+		fstream.on('close', function () {
+			configureParams.headers = excelHelper.readSheet(configureParams.file, configureParams.sheetName).headers;
+			res.render('configure', configureParams);
 		});
-	}
+	});
+	req.busboy.on('field', function (key, value) {
+		if (key === 'sheetName') {
+			configureParams.sheetName = value;
+		}
+	});
+	req.busboy.on('finish', function () {
+		if (!configureParams.sheetName || !gotFile) {
+			res.redirect('/?missingSheetName=' + !configureParams.sheetName + '&missingFile=' + !gotFile);
+		}
+	});
+	req.pipe(req.busboy);
 });
 
 if (!fs.existsSync(uploadPath)) {
